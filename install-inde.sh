cf28a3de9c0bf1ef0d0d2a980f43f676cf4672f6
sistemasinde <74721540+sistemasinde@users.noreply.github.com>
2021-01-05 13:15:49 -0500

    Update install-inde.sh

--- install-inde.sh
+++ install-inde.sh
@@ -1,27 +1,17 @@
 #!/bin/bash
 #DOWNLOADED
-cd /
-cd /tmp/
 wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | apt-key add -
 echo "deb http://deb.anydesk.com/ all main" > /etc/apt/sources.list.d/anydesk-stable.list
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys CB2DE8E5
 echo "deb https://download.onlyoffice.com/repo/debian squeeze main" >> /etc/apt/sources.list.d/onlyoffice.list
-wget https://zoom.us/client/latest/zoom_amd64.deb     #!/bin/bash
+wget https://zoom.us/client/latest/zoom_amd64.deb
 sudo add-apt-repository ppa:nextcloud-devs/client
 sudo apt update
-
-#NEXTCLOUD-CLIENT
-sudo add-apt-repository ppa:nextcloud-devs/client
-
-#ANYDESK
-sudo apt install anydesk
-
+sudo apt-get upgrade
 #INSTALL IT!
-sudo apt-get install onlyoffice-desktopeditors ssh gdebi okular nextcloud-client
+sudo apt-get install anydesk onlyoffice-desktopeditors ssh gdebi okular nextcloud-client nextcloud-desktop
 
 #ZOOM
 sudo apt install ./zoom_amd64.deb
 sudo rm -r zoom_amd64.deb
-#NEXTCLOUD-CLIENT
-sudo apt-get update
-sudo apt-get install nextcloud-client
+
