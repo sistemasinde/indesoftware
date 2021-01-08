fd5372c0f34b368920dc9cfd1089685c725efd41
sistemasinde <74721540+sistemasinde@users.noreply.github.com>
2021-01-08 09:24:55 -0500

    Update install-inde.sh
    Se agregó Google Chrome Stable y se agrego una inea con la variable de nextcloud-client por si no la encuentra, no cancele el resto de instalaciones,

--- install-inde.sh
+++ install-inde.sh
@@ -2,6 +2,9 @@
 #DOWNLOADED
 wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | apt-key add -
 echo "deb http://deb.anydesk.com/ all main" > /etc/apt/sources.list.d/anydesk-stable.list
+echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/chrome-stable.list
+wget https://dl.google.com/linux/linux_signing_key.pub
+sudo apt-key add linux_signing_key.pub
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys CB2DE8E5
 echo "deb https://download.onlyoffice.com/repo/debian squeeze main" >> /etc/apt/sources.list.d/onlyoffice.list
 wget https://zoom.us/client/latest/zoom_amd64.deb
@@ -9,7 +12,9 @@ sudo add-apt-repository ppa:nextcloud-devs/client
 sudo apt update
 sudo apt-get upgrade
 #INSTALL IT!
-sudo apt-get install anydesk onlyoffice-desktopeditors ssh gdebi okular nextcloud-client nextcloud-desktop
+sudo apt-get install anydesk onlyoffice-desktopeditors ssh gdebi okular nextcloud-desktop google-chrome-stable
+sudo apt-get install nextcloud-client 
+sudo apt-get install google-chrome-stable
 
 #ZOOM
 sudo apt install ./zoom_amd64.deb
